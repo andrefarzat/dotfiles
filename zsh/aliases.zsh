c1e6b51c2736b1b34be3f1f208c86a1f32344e82
Andre Farzat <andrefarzat@gmail.com>
2013-11-06 22:52:05 -0500

    Fixing runserver command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,24 @@ alias env_fipro="source ~/Documents/envs/fipro/bin/activate; cdd; cd FichaProces
 alias env_mtlpy="source ~/Documents/envs/mtlpy/bin/activate; cdd; cd mtlpy"
 alias env_parasuco="source ~/Documents/envs/parasuco/bin/activate; cdd; cd parasuco"
 alias env_parasucoextranet="source ~/Documents/envs/parasucoextranet/bin/activate; cdd; cd ParasucoExtranet"
+alias env_linh="source ~/Documents/envs/linh/bin/activate; cdd; cd linh;"
+
+
+runserver(){
+	
+	# Let's first see if there is a manage.py file in the current directory
+	if [ -f './manage.py' ]; then
+        python manage.py runserver 0.0.0.0:8000
+        exit;
+	fi
+
+    # Then, let's try to see if there is a folder bin and a django file inside it
+    if [ -f './bin/django' ]; then
+        python ./bin/django runserver 0.0.0.0:8000
+        exit;
+    fi
+
+    # if reached here, well ...
+    echo "You're not in a django project"
+
+}
