7b953a431d7a1b716ab3ea92509d1ddff1177363
Andre Farzat <andrefarzat@gmail.com>
2013-11-07 11:38:16 -0500

    Updating aliases list

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,15 +3,13 @@
 alias cdd="cd /Library/WebServer/Documents"
 alias python_smtp="python -m smtpd -n -c DebuggingServer localhost:1025"
 
-
-alias env_assembl="source ~/Documents/envs/assembl/bin/activate; cdd; cd assembl"
-alias env_caravan="source ~/Documents/envs/caravan/bin/activate; cdd; cd caravan"
 alias env_fipro="source ~/Documents/envs/fipro/bin/activate; cdd; cd FichaProcesso/trunk/src"
-alias env_mtlpy="source ~/Documents/envs/mtlpy/bin/activate; cdd; cd mtlpy"
-alias env_parasuco="source ~/Documents/envs/parasuco/bin/activate; cdd; cd parasuco"
-alias env_parasucoextranet="source ~/Documents/envs/parasucoextranet/bin/activate; cdd; cd ParasucoExtranet"
-alias env_linh="source ~/Documents/envs/linh/bin/activate; cdd; cd linh;"
 
+projects=('assembl' 'caravan' 'mtlpy' 'parasuco' 'parasucoextranet' 'linh' 'csf')
+for project in "${projects[@]}"
+do
+    alias "env_$project"="source ~/Documents/envs/$project/bin/activate; cdd; cd $project"
+done
 
 runserver(){
 	
