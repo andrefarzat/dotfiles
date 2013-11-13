dc57ca05006c4c5238bc3676d47a92d962e9529d
Andre Farzat <andrefarzat@gmail.com>
2013-11-13 11:56:54 -0500

    Fixing runserver

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,27 +5,25 @@ alias python_smtp="python -m smtpd -n -c DebuggingServer localhost:1025"
 
 alias env_fipro="source ~/Documents/envs/fipro/bin/activate; cdd; cd FichaProcesso/trunk/src"
 
-projects=('assembl' 'caravan' 'mtlpy' 'parasuco' 'parasucoextranet' 'linh' 'csf')
+projects=('assembl' 'caravan' 'mtlpy' 'parasuco' 'parasucoextranet' 'linh' 'csf' 'parasucocorp' 'styleguide' 'prana')
 for project in "${projects[@]}"
 do
     alias "env_$project"="source ~/Documents/envs/$project/bin/activate; cdd; cd $project"
 done
 
 runserver(){
-	
-	# Let's first see if there is a manage.py file in the current directory
-	if [ -f './manage.py' ]; then
+    
+    if [ -f './manage.py' ]; then
+        # Let's first see if there is a manage.py file in the current directory
+        echo './manage.py'
         python manage.py runserver 0.0.0.0:8000
-        exit;
-	fi
-
-    # Then, let's try to see if there is a folder bin and a django file inside it
-    if [ -f './bin/django' ]; then
+    elif [ -f './bin/django' ]; then
+        # Then, let's try to see if there is a folder bin and a django file inside it
+        echo './bin/django'
         python ./bin/django runserver 0.0.0.0:8000
-        exit;
+    else
+        # if reached here, well ...
+        echo "You're not in a django project"
     fi
-
-    # if reached here, well ...
-    echo "You're not in a django project"
 
 }
