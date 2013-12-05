fdc675a5cb003ad5be1bb7688e06bde0bc033e72
Andre Farzat <andrefarzat@gmail.com>
2013-12-05 11:14:23 -0500

    Adding conditionals to update sublime 2 and 3

--- sublime2/update.zsh
+++ sublime2/update.zsh
@@ -1,3 +1,6 @@
-cp -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/ ~/.dotfiles/sublime2/User
+if [ -d ~/Library/Application\ Support/Sublime\ Text\ 2 ]; then
+	
+	cp -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/ ~/.dotfiles/sublime2/User
+	echo "Sublime text 2 settings copied"
 
-echo "Sublime text 2 settings copied"
+fi
--- sublime3/update.zsh
+++ sublime3/update.zsh
@@ -1,3 +1,6 @@
-cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ ~/.dotfiles/sublime3/User
+if [ -d ~/Library/Application\ Support/Sublime\ Text\ 3 ]; then
+	
+	cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ ~/.dotfiles/sublime3/User
+	echo "Sublime text 3 settings copied"
 
-echo "Sublime text 3 settings copied"
+fi
