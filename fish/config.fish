f14732e9b73fc4377e5739edf410654a21973639
Andre Farzat <andrefarzat@gmail.com>
2015-10-06 11:48:31 -0400

    Commit at 06 Oct 2015

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,5 @@
 set PATH ~/.dotfiles/bin $PATH
+
+# powerline-status
+set fish_function_path $fish_function_path "/Users/andrefarzat/Library/Python/2.7/lib/python/site-packages/powerline/bindings/fish"
+powerline-setup
