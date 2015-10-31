0d4b8260b8385b2fad400bd9e463693560dd86c2
Andre Farzat <andrefarzat@gmail.com>
2015-10-30 21:15:49 -0400

    Commit at 30 Oct 2015

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set PATH ~/.dotfiles/bin $PATH
 
+# Postgres app
+set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
+
 # powerline-status
 set fish_function_path $fish_function_path "/Users/andrefarzat/Library/Python/2.7/lib/python/site-packages/powerline/bindings/fish"
 powerline-setup
