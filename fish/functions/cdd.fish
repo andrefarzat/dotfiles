a7c42b8c67c0a852c1dcc2e8b024cc603648e900
Andre Farzat <andrefarzat@gmail.com>
2017-02-05 11:51:45 -0200

    Commit at 05 Feb 2017

--- fish/functions/cdd.fish
+++ fish/functions/cdd.fish
@@ -1,6 +1,11 @@
 function cdd --description "Goes to our projects folder"
-    if test -d ~/Documents/$argv
-        cd ~/Documents/$argv
+
+    if test $argv
+        if test -d ~/Documents/$argv
+            cd ~/Documents/$argv
+        else
+            cd ~/Documents/
+        end
     else
         cd ~/Documents/
     end
