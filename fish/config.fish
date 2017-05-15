d964d705f947beda94bb5a0a4298b47062c9bab0
Andre Farzat <andrefarzat@gmail.com>
2017-05-15 15:06:20 -0300

    Commit at 15 May 2017

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,5 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 # set DATABASE_URL postgres:///(whoami)
 
 # For android studio
-set -g ANDROID_HOME /Users/andrefarzat/Android
-set PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
+#set -g ANDROID_HOME /Users/andrefarzat/Android
+#set PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
